f4cf05178538f8fa16d98e018722f061e33e7e43
Diego Sepulveda <diegosl@hotmail.cl>
2021-06-28 17:28:06 -0400

    twoNumberSum.js solution2

--- dist/twoNumberSum.js
+++ dist/twoNumberSum.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.twoNumberSum = void 0;
+exports.twoNumberSumImproved = exports.twoNumberSum = void 0;
 const numbers = [3, 5, -4, 8, 11, 1, -1, 6];
 const target = 10;
 function twoNumberSum(array, targetSum) {
@@ -25,4 +25,22 @@ function twoNumberSum(array, targetSum) {
     return [];
 }
 exports.twoNumberSum = twoNumberSum;
+function twoNumberSumImproved(array, targetSum) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            const currentINumber = array[i];
+            const currentJNumber = array[j];
+            const checkSum = currentINumber + currentJNumber === targetSum ? true : false;
+            if (!checkSum) {
+                continue;
+            }
+            else {
+                return [currentINumber, currentJNumber];
+            }
+        }
+    }
+    return [];
+}
+exports.twoNumberSumImproved = twoNumberSumImproved;
 console.log(twoNumberSum(numbers, target));
+console.log(twoNumberSumImproved(numbers, target));
