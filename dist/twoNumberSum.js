3c2307e17b730c827f2157072c5f418ef5157940
Diego Sepulveda <diegosl@hotmail.cl>
2021-06-28 18:34:00 -0400

    twoNumberSum.js solution3

--- dist/twoNumberSum.js
+++ dist/twoNumberSum.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.twoNumberSumImproved = exports.twoNumberSum = void 0;
-const numbers = [3, 5, -4, 8, 11, 1, -1, 6];
-const target = 10;
+exports.twoNumberSumWithHashTable = exports.twoNumberSumImproved = exports.twoNumberSum = void 0;
+const numbers = [3, 5, -4, 8, 11, 1, -1, 6, 2, 4, -5, -6, 15, 21, 13, -7, 22, -22, 45, -88, -17, -103, -100];
+const target = -203;
 function twoNumberSum(array, targetSum) {
     for (let i = 0; i < array.length; i++) {
         const currentINumber = array[i];
@@ -31,10 +31,7 @@ function twoNumberSumImproved(array, targetSum) {
             const currentINumber = array[i];
             const currentJNumber = array[j];
             const checkSum = currentINumber + currentJNumber === targetSum ? true : false;
-            if (!checkSum) {
-                continue;
-            }
-            else {
+            if (checkSum) {
                 return [currentINumber, currentJNumber];
             }
         }
@@ -42,5 +39,26 @@ function twoNumberSumImproved(array, targetSum) {
     return [];
 }
 exports.twoNumberSumImproved = twoNumberSumImproved;
+function twoNumberSumWithHashTable(array, targetSum) {
+    const hashTable = {};
+    for (let number of array) {
+        let potencialMatch = targetSum - number;
+        if (potencialMatch in hashTable) {
+            return [potencialMatch, number];
+        }
+        else {
+            hashTable[number] = true;
+        }
+    }
+    return [];
+}
+exports.twoNumberSumWithHashTable = twoNumberSumWithHashTable;
+console.time("twoNumberSum");
 console.log(twoNumberSum(numbers, target));
+console.timeEnd("twoNumberSum");
+console.time("twoNumberSumImproved");
 console.log(twoNumberSumImproved(numbers, target));
+console.timeEnd("twoNumberSumImproved");
+console.time("twoNumberSumWithHashTable");
+console.log(twoNumberSumWithHashTable(numbers, target));
+console.timeEnd("twoNumberSumWithHashTable");
