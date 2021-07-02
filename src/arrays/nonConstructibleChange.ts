3a85323b7e9385660d3e93017c8ddbc5283870cb
Diego Sepulveda <diegosl@hotmail.cl>
2021-07-02 17:17:49 -0400

    nonConstructibleChange comments added

--- src/arrays/nonConstructibleChange.ts
+++ src/arrays/nonConstructibleChange.ts
@@ -22,17 +22,23 @@ const coins = [5, 7, 1, 1, 2, 3, 22];
 //const coins = [1, 2, 4];
 
 export function nonConstructibleChange(coins: number[]) {
-  let maxPossibleChange = 0;
-  let sortedCoins = [...coins].sort((a, b) => a - b);
-
-  for (let coin of sortedCoins) {
-    if (coin > maxPossibleChange + 1) {
-      return maxPossibleChange + 1;
+  // we sort the array if we want O(I)S or if we need to copy then we would have O(n)S
+  coins.sort((a, b) => a - b);
+  //we set the min current change
+  let currentChange = 0;
+  //iterate over each coin in the coins array
+  for (let coin of coins) {
+    // if the current coint is bigger than the current possible coin we return due is not possible to create a bigger change
+    // with the coins we have
+    if (coin > currentChange + 1) {
+      return currentChange + 1;
+      // otherwise we add the currentcoint to current possible change
     } else {
-      maxPossibleChange += coin;
+      currentChange += coin;
     }
   }
-  return maxPossibleChange + 1;
+  // if we hit the end of the array then is not possible to create a bigger change with the coins we have
+  return currentChange + 1;
 }
 
 console.log(nonConstructibleChange(coins)); // O(n*log(n)) T and O(I) S
