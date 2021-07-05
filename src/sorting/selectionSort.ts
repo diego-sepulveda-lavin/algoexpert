9faf1ddbfca0212be43eb448c8fdc3697df8ea14
Diego Sepulveda <diegosl@hotmail.cl>
2021-07-05 14:43:02 -0400

    selectionSort comments added

--- src/sorting/selectionSort.ts
+++ src/sorting/selectionSort.ts
@@ -15,22 +15,22 @@
 const array = [8, 5, 2, 9, 5, 6, 3];
 
 export function selectionSort(array: number[]) {
-  let arrangedI = 0;
+  let arrangingI = 0;
   let currI = 0;
   let smallestNumI = 0;
   let arrLen = array.length;
 
   // Loop until currI reaches the end of the array and the arranged pointer reaches the end of array
-  while (currI < arrLen && arrangedI < arrLen) {
+  while (currI < arrLen && arrangingI < arrLen) {
     // Check whether current iterated number is smaller than smallest, if so, save this new smallest index
     array[currI] < array[smallestNumI] && (smallestNumI = currI);
 
-    // if currI reaches end of array, swap the smallest number found, for the current number of arrangedI
+    // if currI reaches end of array, swap the smallest number found, for the current number of arrangingI
     if (currI === arrLen - 1) {
-      swap(arrangedI, smallestNumI, array);
-      arrangedI++;
-      currI = arrangedI;
-      smallestNumI = arrangedI;
+      swap(arrangingI, smallestNumI, array);
+      arrangingI++; //go to next index to be arranged
+      currI = arrangingI;  // equal other pointers to start again
+      smallestNumI = arrangingI; // equal other pointers to start again
     }
     currI++;
   }
@@ -43,4 +43,4 @@ function swap(i: number, j: number, array: number[]) {
   array[j] = aux;
 }
 
-console.log(selectionSort(array));
+console.log(selectionSort(array)); // O(n²)T and O(I)S
