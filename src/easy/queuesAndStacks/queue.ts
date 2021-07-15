87e906ccd914035c643a83d24956c0af3e4e3f3c
Diego Sepulveda <diegosl@hotmail.cl>
2021-07-15 18:16:53 -0400

    queue data structure added

--- src/easy/queuesAndStacks/queue.ts
+++ src/easy/queuesAndStacks/queue.ts
@@ -9,5 +9,55 @@ class QueueNode {
 }
 
 class Queue {
-  constructor() {}
+  private front: QueueNode | null;
+  private back: QueueNode | null;
+  private length: number;
+
+  constructor() {
+    this.front = null;
+    this.back = null;
+    this.length = 0;
+  }
+
+  enqueue(val: number | string) {
+    const newNode = new QueueNode(val);
+    if (this.length === 0) {
+      this.front = newNode;
+      this.back = newNode;
+    } else {
+      this.back!.next = newNode;
+      this.back = newNode;
+    }
+    this.length++;
+  }
+
+  dequeue() {
+    if (this.length === 0) return null;
+    if (this.length === 1) {
+      this.back = null;
+    }
+
+    const removedNode = this.front;
+    this.front = this.front!.next;
+    this.length--;
+    return removedNode?.value;
+  }
+
+  getLength() {
+    return this.length;
+  }
+
+  getFront() {
+    return this.front?.value;
+  }
+  getBack() {
+    return this.back?.value;
+  }
 }
+
+let myQueue = new Queue();
+myQueue.enqueue("a");
+myQueue.enqueue("b");
+myQueue.enqueue("c");
+console.log(myQueue.getFront());
+console.log(myQueue.getBack());
