9fa5b1e946ac1177242588f611de45bf60a8f9cc
Diego Sepulveda <diegosl@hotmail.cl>
2021-07-21 15:55:03 -0400

    add depthFirstTraversal to graphTraversals

--- src/easy/graphs/graphTraversals.ts
+++ src/easy/graphs/graphTraversals.ts
@@ -7,16 +7,16 @@ export class GraphNode {
     this.edgesList = [];
   }
 
-  connect(node: GraphNode) {
+  connect(node: GraphNode): void {
     this.edgesList.push(node);
     node.edgesList.push(this);
   }
 
-  getAdjacentNodes() {
+  getAdjacentNodes(): GraphNode[] {
     return this.edgesList;
   }
 
-  isConnected(node: GraphNode) {
+  isConnected(node: GraphNode): boolean {
     return this.edgesList.some((edge) => edge === node);
   }
 }
@@ -28,11 +28,11 @@ export class Graph {
     this.nodes = [...nodes];
   }
 
-  addNode(node: GraphNode) {
+  addNode(node: GraphNode): void {
     this.nodes.push(node);
   }
 
-  breadthFirstTraversal(start: GraphNode) {
+  breadthFirstTraversal(start: GraphNode): void {
     let queue = [start];
     let visitedNodes = new Set();
     visitedNodes.add(start);
@@ -49,6 +49,19 @@ export class Graph {
       });
     }
   }
+
+  depthFirstTraversal(start: GraphNode, visited = new Set()): void {
+    //keep traversing deeply until you hit a leaf node or end node
+
+    console.log(start.value);
+
+    visited.add(start);
+    for (const edge of start.edgesList) {
+      if (!visited.has(edge)) {
+        this.depthFirstTraversal(edge, visited);
+      }
+    }
+  }
 }
 
 const DFW = new GraphNode("DFW");
@@ -56,25 +69,33 @@ const JFK = new GraphNode("JFK");
 const LAX = new GraphNode("LAX");
 const HNL = new GraphNode("HNL");
 const SAN = new GraphNode("SAN");
+const HKG = new GraphNode("HKG");
 const EWR = new GraphNode("EWR");
 const BOS = new GraphNode("BOS");
 const MIA = new GraphNode("MIA");
 const MCO = new GraphNode("MCO");
 const PBI = new GraphNode("PBI");
 
-const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI]);
+const graph = new Graph([DFW, JFK, LAX, HNL, SAN, HKG, EWR, BOS, MIA, MCO, PBI]);
 
 DFW.connect(LAX);
 DFW.connect(JFK);
-JFK.connect(BOS);
-JFK.connect(MIA);
 LAX.connect(HNL);
 LAX.connect(EWR);
 LAX.connect(SAN);
+SAN.connect(HKG);
+JFK.connect(BOS);
+JFK.connect(MIA);
 MIA.connect(MCO);
 MIA.connect(PBI);
 MCO.connect(PBI);
 
 // Time: O(v+e) where v is the number of vertices and e the number of edges
 // Space: O(v)
-graph.breadthFirstTraversal(SAN);
+console.log("breadthFirstTraversal");
+graph.breadthFirstTraversal(DFW);
+
+// Time: O(v+e) where v is the number of vertices and e the number of edges
+// Space: O(v)
+console.log("depthFirstTraversal");
+graph.depthFirstTraversal(DFW);
