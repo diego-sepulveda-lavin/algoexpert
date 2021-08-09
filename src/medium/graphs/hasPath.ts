f6e9f2b798772f3552f03bd976546f5b31f3b406
Diego Sepulveda <diegosl@hotmail.cl>
2021-08-09 17:53:32 -0400

    add hasPathrec function for graph

--- src/medium/graphs/hasPath.ts
+++ src/medium/graphs/hasPath.ts
@@ -15,7 +15,7 @@ const hasPath = (graph: Graph, source: string, destination: string): boolean => 
   const stack = [source];
   while (stack.length > 0) {
     const currNode = stack.pop();
-    console.log(currNode);
+    //console.log(currNode);
     if (currNode === destination) return true;
     if (currNode) {
       const neighbors = graph[currNode];
@@ -27,4 +27,23 @@ const hasPath = (graph: Graph, source: string, destination: string): boolean => 
   return false;
 };
 
-console.log(hasPath(graph, "j", "h"));
+const hasPathRec = (graph: Graph, source: string, destination: string): boolean => {
+  if (source === destination) return true;
+  const neighbors = graph[source];
+  for (const neighbor of neighbors) {
+    if (hasPathRec(graph, neighbor, destination) === true) {
+      return true;
+    }
+  }
+  return false;
+};
+
+console.log(hasPath(graph, "j", "h")); //true
+console.log(hasPath(graph, "f", "k")); // true
+console.log(hasPath(graph, "g", "k")); // false
+console.log(hasPath(graph, "k", "f")); // false
+console.log("----------------");
+console.log(hasPathRec(graph, "j", "h")); //true
+console.log(hasPathRec(graph, "f", "k")); // true
+console.log(hasPathRec(graph, "g", "k")); // false
+console.log(hasPathRec(graph, "k", "f")); // false
